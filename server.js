65d36140e499338126f0f23567f529cd8ebf3011
k-sang-soo <wsckm1@google.com>
2023-05-22 21:39:13 +0900

    여러 타입 데이터 생성

--- server.js
+++ server.js
@@ -3,20 +3,31 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 //apollo 서버에 data type을 꼭 명시해줘야함
-const typeDefs = `#graphql
-  type Query {
-    hello: String
-  }
+const typeDefs = gql`
+    type User {
+        id: ID
+        username: String
+    }
+    type Tweet {
+        id: ID
+        text: String
+        author: User
+    }
+    type Query {
+        allTweets: [Tweet]
+        tweet(id: ID): Tweet
+    }
 `;
 
-const resolvers = {
-    Query: {
-        hello: () => 'world',
-    },
-};
+// const resolvers = {
+//     Query: {
+//         hello: () => 'world',
+//     },
+// };
+
 const server = new ApolloServer({
     typeDefs,
-    resolvers,
+    // resolvers,
 });
 
 const { url } = await startStandaloneServer(server);
