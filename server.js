8959d741d56ce1833b480a50b7f1fbcb5828962c
k-sang-soo <wsckm1@google.com>
2023-06-03 18:55:06 +0900

    Tweet author에 User 관계 만들기, doc 작성법, REST API -> GraphQL 변경하는 방법

--- server.js
+++ server.js
@@ -7,36 +7,86 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 // ! 는 값이 required 라는 뜻이다. 만약 없다면 설정된 타입 또는 Null 이라는 뜻
 
 //dumy data
-const tweets = [
+let tweets = [
     {
         id: '1',
-        text: 'test1',
+        text: 'first one!',
+        userId: '2',
     },
     {
         id: '2',
-        text: 'test2',
+        text: 'second one',
+        userId: '1',
+    },
+];
+
+let users = [
+    {
+        id: '1',
+        firstName: 'nico',
+        lastName: 'las',
+    },
+    {
+        id: '2',
+        firstName: 'Elon',
+        lastName: 'Mask',
     },
 ];
 
 const typeDefs = gql`
     type User {
         id: ID!
-        username: String!
         firstName: String!
-        lastName: String
+        lastName: String!
+        """
+        Is the sum of firstName + lastName as a string
+        """
+        fullName: String!
     }
+    """
+    Tweet object represents a resource for  a Tweet
+    """
     type Tweet {
         id: ID!
         text: String!
         author: User
     }
     type Query {
+        allMovies: [Movie!]!
+        allUsers: [User!]!
         allTweets: [Tweet!]!
         tweet(id: ID!): Tweet
+        movie(id: String!): Movie
     }
     type Mutation {
         postTweet(text: String!, userId: ID!): Tweet!
+        """
+        Deletes a Tweet if found, else returns false
+        """
         deleteTweet(id: ID!): Boolean!
+    }
+    type Movie {
+        id: Int!
+        url: String!
+        imdb_code: String!
+        title: String!
+        title_english: String!
+        title_long: String!
+        slug: String!
+        year: Int!
+        rating: Float!
+        runtime: Float!
+        genres: [String]!
+        summary: String
+        description_full: String!
+        synopsis: String
+        yt_trailer_code: String!
+        language: String!
+        background_image: String!
+        background_image_original: String!
+        small_cover_image: String!
+        medium_cover_image: String!
+        large_cover_image: String!
     }
 `;
 
@@ -50,6 +100,22 @@ const resolvers = {
         tweet(root, { id }) {
             return tweets.find((tweet) => tweet.id === id);
         },
+        allUsers() {
+            console.log('allUsers called!');
+            return users;
+        },
+        allMovies() {
+            return fetch('https://yts.mx/api/v2/list_movies.json')
+                .then((r) => r.json())
+                .then((json) => json.data.movies);
+        },
+        movie(_, { id }) {
+            return fetch(
+                `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`,
+            )
+                .then((r) => r.json())
+                .then((json) => json.data.movie);
+        },
     },
     Mutation: {
         postTweet(_, { text, userId }) {
@@ -59,6 +125,22 @@ const resolvers = {
             };
             tweets.push(newTweet);
             return newTweet;
+        },
+        deleteTweet(_, { id }) {
+            const tweet = tweets.find((tweet) => tweet.id === id);
+            if (!tweet) return false;
+            tweets = tweets.filter((tweet) => tweet.id !== id);
+            return true;
+        },
+    },
+    User: {
+        fullName({ firstName, lastName }) {
+            return `${firstName} ${lastName}`;
+        },
+    },
+    Tweet: {
+        author({ userId }) {
+            return users.find((user) => user.id === userId);
         },
     },
 };
